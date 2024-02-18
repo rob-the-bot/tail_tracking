13f612153df4dfb035ce3051f09a9da98eb0b2d2
Robert Wong <robertwong928@gmail.com>
2024-02-18 11:39:30 -0600

    added file for visualizing raw and processed

---
+++ view.py
@@ -0,0 +1,38 @@
+# %% imports
+from pathlib import Path
+
+import numpy as np
+import napari
+import dask.array as da
+import h5py
+import tifffile
+
+# %%
+parent_folder = Path("..") / "20231102" / "tail"
+images = tifffile.imread(
+    parent_folder
+    / "231102_pd60_h2bGCAMP7f_6dpf_63Hz_tail_natural_sleep_2p_1_MMStack_Pos0.ome.tif",
+)
+
+MIN, MAX = 3000, 20000
+images = np.clip(images[1:, 10:73, :68], MIN, MAX)
+
+# %%
+
+%store -r bgd
+
+for y in range(35, 45):
+    for x in range(60):
+        bgd[y, x] = np.random.randn() * 500 + 3000
+
+images2 = images - bgd
+
+# %%
+
+viewer = napari.Viewer()
+viewer.add_image(images, contrast_limits=[3000, 20000])
+viewer.add_image(bgd)
+viewer.add_image(images2, contrast_limits=[3000, 20000])
+napari.run()
+
+# %%
